baba31fa6ac2791633d2fc28c2f808c7af9db1f9
Kristyna <xpekark1@fi.muni.cz>
2018-05-05 19:54:53 +0200

    refactor search + find category and media methods

--- src/main/java/cz/muni/pb138project/XMLDatabaseManager.java
+++ src/main/java/cz/muni/pb138project/XMLDatabaseManager.java
@@ -11,55 +11,56 @@ public interface XMLDatabaseManager {
     /**
      * Creates a new media category and adds it to the database.
      * @param category name of the category to be created
-     * @return
      */
-    String createCategory(String category);
+    void createCategory(String category);
 
 
     /**
      * Removes a media category from the database.
      * @param category name of the category to be deleted
-     * @return
      */
-    String deleteCategory(String category);
+    void deleteCategory(String category);
 
 
     /**
      * Searches for media that belong to a certain category.
      * @param category to which the media belong
-     * @return
+     * @return all media that belong to one, specified category
      */
     String searchMediaByCategory(String category);
+
+
+    /**
+     * Finds all media categories that currently exist in the database.
+     * @return all media categories in the database
+     */
+    String findAllCategories();
 
 
     /**
      * Creates a new medium entry and adds it to a given category in the database.
      * @param medium to be added to a certain category
      * @param category to add the media to
-     * @return
      */
-    String addMediumToCollection(String medium, String category);
+    void addMediumToCollection(String medium, String category);
 
 
     /**
      * Moves a medium from one media category to another.
      * @param medium name of the medium to be moved
-     * @param category to which the media should be moved
-     * @return
      */
-    String moveMediumToAnotherCategory(String medium, String category);
+    void moveMediumToAnotherCategory(String medium, String category);
 
 
     /**
      * Deletes a selected medium from the database.
      * @param media name of the medium to be deleted
-     * @return
      */
-    String deleteMediumFromCollection(String media);
+    void deleteMediumFromCollection(String media);
 
 
     /**
-     * Searches for media with the given attribute.
+     * Searches for media with given attributes.
      * @param label
      * @param genres to which the media belong
      * @param properties
