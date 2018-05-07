da2c17e671b4b02b96ddd2b4c2b352680fbe37f6
sylen1 <l.labaj18@gmail.com>
2018-05-07 13:27:34 +0200

    controllers implemented

--- src/main/java/cz/muni/pb138project/CategoriesController.java
+++ src/main/java/cz/muni/pb138project/CategoriesController.java
@@ -1,38 +1,55 @@
 package cz.muni.pb138project;
 
 import cz.muni.pb138project.forms.CreateCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
+import javax.xml.transform.TransformerException;
+import javax.xml.xquery.XQException;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class CategoriesController {
 
+    @Autowired
+    private XSLTransformer transformer;
+
+    @Autowired
+    private XMLDatabaseManager databaseManager;
+
     @RequestMapping("/manage_categories")
-    public String manageCategories(Model model) {
+    public String manageCategories(Model model) throws TransformerException, XQException {
         model.addAttribute("createCategoryForm", new CreateCategory());
+        addMenu(model);
         return "manage_categories";
     }
 
     @RequestMapping(value = "/create_category", method = POST)
     public String createCategory(@Valid @ModelAttribute("createCategoryForm") CreateCategory createCategory,
                                  BindingResult bindingResult,
-                                 Model model) {
-        System.out.println("bindingResult.hasErrors() = " + bindingResult.hasErrors());
-        System.out.println(createCategory);
+                                 Model model) throws TransformerException, XQException {
+
+        addMenu(model);
+
         if (bindingResult.hasErrors()) {
             model.addAttribute("createCategoryForm", createCategory);
             return "manage_categories";
         }
+        // TODO: test unique
+//        databaseManager.createCategory(createCategory.getName());
         return "redirect:/manage_categories";
     }
+
+    private void addMenu(Model model) throws TransformerException, XQException {
+        String xmlCategories = databaseManager.findAllCategories();
+        String htmlCategories = transformer.transform(xmlCategories, "categories.xsl");
+        model.addAttribute("categoryMenu", htmlCategories);
+    }
+
 }
--- src/main/java/cz/muni/pb138project/HomeController.java
+++ src/main/java/cz/muni/pb138project/HomeController.java
@@ -1,25 +1,77 @@
 package cz.muni.pb138project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.xml.transform.TransformerException;
+import javax.xml.xquery.XQException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private XSLTransformer transformer;
+
+    @Autowired
+    private XMLDatabaseManager databaseManager;
+
     @RequestMapping("")
-    public String index(Model model) {
+    public String index(@RequestParam(required = false) String category,
+                        Model model) throws TransformerException, XQException {
+
+        addMenu(model);
+        category = (category != null) ? category : databaseManager.getFirstCategory();
+
+        String xmlEntries = databaseManager.searchMediaByCategory(category);
+        String htmlEntries = transformer.transform(xmlEntries, "media-entries.xsl");
+        model.addAttribute("mediaEntries", htmlEntries);
+//        model.addAttribute("category", category);
         return "index";
     }
 
     @RequestMapping(value = "/search", method = POST)
-    public String search(@RequestBody MultiValueMap<String,String> formData) {
-        System.out.println(formData);
-        return "redirect:/";
+    public String search(@RequestBody MultiValueMap<String,String> formData, Model model) throws TransformerException, XQException {
+        addMenu(model);
+
+        String label = formData.getFirst("label");
+        String genres = formData.getFirst("tags");
+        String[] genresArray = genres.isEmpty() ? null : genres.split(" ");
+
+//        String category =
+        Map<String, String> properties = new HashMap<>();
+        List<String> keys = formData.get("prop-keys");
+        List<String> values = formData.get("prop-values");
+        for (int i = 0; i < keys.size(); i++) {
+            if (!keys.get(i).isEmpty() && !values.get(i).isEmpty()) {
+                properties.put(keys.get(i), values.get(i));
+            }
+        }
+
+        String xmlMedia = databaseManager.searchMedia(label.isEmpty() ? null : label,
+                genresArray,
+                properties.isEmpty() ? null : properties,
+                null);
+
+        String htmlMedia = transformer.transform(xmlMedia, "media-entries.xsl");
+        model.addAttribute("mediaEntries", htmlMedia);
+
+        return "index";
+    }
+
+    private void addMenu(Model model) throws TransformerException, XQException {
+        String xmlCategories = databaseManager.findAllCategories();
+        String htmlCategories = transformer.transform(xmlCategories, "categories.xsl");
+        model.addAttribute("categoryMenu", htmlCategories);
     }
 
 }
